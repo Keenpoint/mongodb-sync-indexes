d97c6d632888067cac3782273476da017effac2e
Danilo Augusto <daniloaugusto.ita16@gmail.com>
2015-07-30 11:46:32 +0200

    Beta version

--- syncIndexes.js
+++ syncIndexes.js
@@ -1,5 +1,150 @@
-var syncIndexes = function (indexes, collection, options, callback) {
+var MongoClient = require("mongodb").MongoClient,
+    assert = require("assert"),
+    _ = require("lodash"),
+    async = require('async');
 
+//TODO: second style (pass db and indexes from multiple collections)
+
+var toIgnoreInArray = ["background", "dropUps"],
+    toIgnoreInDatabase = ["v", "ns"],
+    toIgnoreIfUndefined = ["name"];
+
+
+var cleanIndexes = function(indexesToClean, dirty){
+    return _.map(indexesToClean, function(indexToClean){
+        return _.omit(indexToClean, dirty);
+    });
+};
+
+var dropIndexes = function(indexesToDrop, collection, callback){
+
+    var tasks = [];
+
+    _.map(indexesToDrop, function(indexToDrop){
+        tasks.push(
+            function(_callback){
+                collection.dropIndex(indexToDrop.key, function(err){
+                    assert.equal(null, err);
+
+                    console.log("Dropped index " + indexToDrop.name + ".");
+
+                    _callback();
+                });
+            }
+        )
+    });
+
+    async.parallel(
+        tasks,
+        function(err){
+            assert.equal(err);
+            callback();
+        }
+    );
+};
+
+var createIndexes = function(indexesToCreate, collection, callback){
+
+    var tasks = [];
+
+    _.map(indexesToCreate, function(indexToCreate){
+            tasks.push(
+                function(_callback){
+                    collection.createIndex(indexToCreate.key, function(err, indexName){
+                        assert.equal(null, err);
+
+                        console.log("Created index " + indexName + ".");
+
+                        _callback();
+                    });
+                }
+            );
+        }
+    );
+
+    async.parallel(
+        tasks,
+        function(err){
+            assert.equal(err);
+            callback();
+        }
+    );
+};
+
+var isEqual = function(index1, index2, positionArray, toIgnoreIfUndefined){
+
+    var indexArray = positionArray == 1 ? index1 : index2,
+        indexCollection = positionArray == 1 ? index2 : index1;
+
+    var toIgnore = _.chain(toIgnoreIfUndefined)
+        .map(function(_toIgnoreIfUndefined){
+            if (indexArray[_toIgnoreIfUndefined] === undefined) return _toIgnoreIfUndefined;
+        })
+        .compact()
+        .value();
+
+    indexCollection = _.omit(indexCollection, toIgnore);
+
+    return _.isEqual(indexArray, indexCollection);
+};
+
+var difference = function(indexes, indexesToKeep, positionArray, toIgnoreIfUndefined){
+
+    return _.chain(indexes)
+        .map(function(index){
+
+            var presentInArray = false;
+            _.map(indexesToKeep, function(indexToKeep){
+                if(isEqual(index, indexToKeep, positionArray, toIgnoreIfUndefined)) presentInArray = true;
+            });
+            if(!presentInArray) return index;
+        })
+        .compact()
+        .value();
+};
+
+//For the moment, collection is a string
+var syncIndexes = function(indexesArray, url, collectionName, options, callback){
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, db){
+
+        assert.equal(null, err);
+        console.log("Correctly connected to server. \n");
+
+        var collection = db.collection(collectionName);
+
+        collection.indexes(function(err, indexesCollection){
+            assert.equal(err, null);
+
+            var cleanIndexesCollection = cleanIndexes(indexesCollection, toIgnoreInDatabase);
+            var cleanIndexesArray= cleanIndexes(indexesArray, toIgnoreInArray);
+
+            var indexesToDrop = difference(cleanIndexesCollection, cleanIndexesArray, 2, toIgnoreIfUndefined);
+            var indexesToCreate = difference(cleanIndexesArray, cleanIndexesCollection, 1, toIgnoreIfUndefined);
+
+            console.log(indexesToDrop);
+            console.log(indexesToCreate);
+
+            async.series(
+                [
+                    function(_callback){
+                        dropIndexes(indexesToDrop, collection, _callback);
+                    },
+                    function(_callback){
+                        createIndexes(indexesToCreate, collection, _callback);
+                    }
+                ],
+                function(err){
+                    assert.equal(err, null);
+                    db.close();
+                }
+            );
+
+            return callback();
+        });
+
+    });
 };
 
 module.exports = syncIndexes;
--- test/test.js
+++ test/test.js
@@ -1,24 +1,47 @@
 'use strict';
 
-var syncIndexes = require('../syncIndexes');
-var chai = require ('chai');
-var expect = chai.expect;
-//var _ = require('lodash');
+var MongoClient = require('mongodb').MongoClient,
+    syncIndexes = require('../syncIndexes'),
+    assert = require('assert'),
+    chai = require ('chai'),
+    expect = chai.expect,
+    _ = require('lodash'),
+    async = require('async');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/mr_demo';
 
 describe('Check sync between first array and collection.', function () {
 
     it('Delete from collection indexes absent in array.', function (done) {
 
-        //Verification
-        expect(result).to.deep.equal(correctAnswer);
+        var arrayOfIndexes1 = require('./arrayOfIndexes1.json');
 
-        done();
+        MongoClient.connect(url, function(err, db){
+            assert.equal(err, null);
+
+            var collection = db.collection('us_economic_assistance');
+
+            collection.dropIndexes(function(err){
+                assert.equal(err, null);
+
+                collection.createIndex({country_name: 1}, function(err, indexName){
+                    assert.equal(err, null);
+
+                    syncIndexes(arrayOfIndexes1, url, 'us_economic_assistance', '', done);
+                });
+            });
+        });
+
+        //Verification
+        //expect(result).to.deep.equal(correctAnswer);
+
     });
 
     it('Insert in collection indexes present in array but not in collection.', function (done) {
 
         //Verification
-        expect(result).to.deep.equal(correctAnswer);
+        //expect(result).to.deep.equal(correctAnswer);
 
         done();
     });
